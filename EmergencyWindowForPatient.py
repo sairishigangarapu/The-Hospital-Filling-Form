4467d12cfe91fc99882a23d7b2a18155de19aa05
Sai Rishi Ganagarapu <72331970+sairishigangarapu@users.noreply.github.com>
2020-11-30 11:51:09 +0530

    Add files via upload

---
+++ EmergencyWindowForPatient.py
@@ -0,0 +1,30 @@
+from tkinter import*
+import tkinter.font as font
+import os
+root = Tk()
+root.geometry("1600x1200")
+root.title('LETS SIGN IN ')
+root.config(bg="#00b4f5")
+myFont = font.Font(family='Times New Roman', size=14, weight='bold')
+Label_0 = Label(root, text="PLEASE FILL THE DETAILS;MAY GOD BE WITH YOU",width=50,font=('Times New Roman',20,'bold'),bg="#00b4f5",fg="black").pack(anchor = CENTER,padx = 10, pady = 5)
+
+label_1= Label(root, text="PLACE OF ACCCIDENT (AREA,CITY,PINCODE)",width=40,font=myFont,bg="#948faf",fg="black").pack(anchor = CENTER,padx = 10, pady = 25)
+
+entry_0=Entry(root, font=myFont).pack(anchor = CENTER,padx = 0, pady = 0)
+
+label_1= Label(root, text="AGE OF THE PERSON WHO IS IN EMERGENCY",width=40,font=myFont,bg="#948faf",fg="black").pack(anchor = CENTER,padx = 10, pady = 25)
+
+entry_0=Entry(root, font=myFont) .pack(anchor = CENTER,padx = 10, pady = 25)
+
+label_1= Label(root, text="APPROXIMATE IS ALSO OKAY",width=40,font=('Times New Roman',10,'bold'),bg="#948faf",fg="black").pack(anchor = CENTER,padx = 10, pady = 5)
+
+btn = Button(root, text='SEND THE SOS', width=20,bg="#948faf",fg='black',font=myFont,activebackground='lime',border = 0).pack(anchor = CENTER,padx = 10, pady = 25)
+def ok():
+    root = Tk()
+frame =Frame(root)
+frame.pack()
+def openFormFirstPage():
+    os.system('python FormFirstPage.py' )
+btn = Button(root, text='(-BACK', width=20,bg="blue",fg='white',font=myFont,activebackground='lime',border = 0,command=openFormFirstPage).pack(anchor = CENTER,padx = 10)
+
+root.mainloop()
