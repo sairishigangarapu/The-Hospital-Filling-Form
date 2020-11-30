0f1f3d3f2e5269042d53408d2585c0d156ec4957
Sai Rishi Ganagarapu <72331970+sairishigangarapu@users.noreply.github.com>
2020-11-30 11:48:38 +0530

    Update login third page.py

--- login third page.py
+++ login third page.py
@@ -12,8 +12,8 @@ ttk.Label(window, text = "PLEASE ENTER THE DETAILS",
 
 ttk.Label(window, text = "PLEASE SELECT THE TYPE OF PROBLEM :", 
            background = '#948faf', foreground ="black",  
-          font = ("Times New Roman", 10)).grid(column = 25, 
-          row = 1, padx = 10, pady = 25) 
+          font = ("Times New Roman", 13)).grid(column = 25, 
+          row =1 , pady = 25) 
   
 # Combobox creation 
 n = tk.StringVar() 
@@ -37,7 +37,7 @@ monthchoosen.current()
 
 ttk.Label(window, text = "SPECIFY THE TYPE OF PROBLEM :",  
           background = '#948faf', foreground ="black",  
-          font = ("Times New Roman", 13)).grid(row = 2, column = 25,padx = 10, pady = 25)
+          font = ("Times New Roman", 13)).grid(row = 2, column = 25, pady = 25)
 
 e1 = tk.Entry(window,width='30')
 e1.grid(row=2, column=35)
@@ -68,7 +68,7 @@ monthchoosen['values'] = (' general checkup',
                           ' first aid',
                           'Others' ) 
   
-monthchoosen.grid(column = 35, row = 6) 
+monthchoosen.grid(column = 35, row = 4) 
 monthchoosen.current()
 
 ttk.Label(window, text = "SPECIFY THE REASON FOR VISIT :",  
@@ -82,14 +82,39 @@ ttk.Label(window, text = "not for those who choose the specific reason for visit
           background = '#00b4f5', foreground ="black",  
           font = ("Times New Roman", 10)).grid(row = 8, column = 25)
 
+ttk.Label(window, text = " ENTER FINACIAL  DETAILS",  
+          background = '#00b4f5', foreground ="black",  
+          font = ("Times New Roman", 20,'bold')).grid(row = 10, column = 0)
+
+ttk.Label(window, text = "MODE OF PAYMENT",  
+          background = '#948faf', foreground ="black",  
+          font = ("Times New Roman", 13)).grid(row = 12, column = 23) 
+
+ttk.Radiobutton(window, text="card",value=1,).grid(row=12,column=25)
+ttk.Radiobutton(window, text="cash",value=2).grid(row=12,column=26)       
+ttk.Radiobutton(window, text="online transaction",value=3).grid(row=13,column=25,pady=15)  
+ttk.Radiobutton(window, text="check",value=4,).grid(row=13,column=26,pady=15)       
+
+
+ttk.Label(window, text = "DO YOU HAVE INSURANCE?",  
+          background = '#948faf', foreground ="black",  
+          font = ("Times New Roman", 13)).grid(row =75 ,column = 23,pady=20)
+
+e1 = tk.Entry(window,width='30')
+e1.grid(column=25 , row=75)
+
+ttk.Label(window, text = "IF YES PLEASE TELL BANK NAME AND BRANCH",  
+          background = '#948faf', foreground ="black",  
+          font = ("Times New Roman", 13)).grid(row =80,column = 23,pady=15)
+
+e1 = tk.Entry(window,width='30')
+e1.grid(column=25 , row=80)
+
 button = tk.Button(window, 
-                   text="NEXT", 
+                   text="SUBMIT", 
                    fg="white",
                    bg = "blue",
                    activebackground="lime",
                    border = '3')
-
-
-button.grid(row=12, column=65)
-
+button.grid(row=100, column=65)
 window.mainloop()
